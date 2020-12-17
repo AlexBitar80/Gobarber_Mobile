5bac42556f387a4e9b012003519363902b033732
Alexandre Bitar <joabitar76@gmail.com>
2020-12-17 00:18:48 -0300

    feat: added main file of SignUp screen

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,8 +1,91 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+  const [keyboardOpen, setKeyboardOpen] = useState(false);
+
+  const _keyboardShown = useCallback(() => setKeyboardOpen(true), []);
+  const _keyboardHidden = useCallback(() => setKeyboardOpen(false), []);
+
+  useEffect(() => {
+    if (Platform.OS !== 'ios') {
+      Keyboard.addListener('keyboardDidShow', _keyboardShown);
+    }
+
+    return () => {
+      Keyboard.removeListener('keyboardDidShow', _keyboardShown);
+    };
+  }, [_keyboardShown]);
+
+  useEffect(() => {
+    if (Platform.OS !== 'ios') {
+      Keyboard.addListener('keyboardDidHide', _keyboardHidden);
+    }
+
+    return () => {
+      Keyboard.removeListener('keyboardDidHide', _keyboardHidden);
+    };
+  }, [_keyboardHidden]);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('Show');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {(Platform.OS === 'ios' || !keyboardOpen) && (
+        <BackToSignIn onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={20} color="#FFF" />
+          <BackToSignInText>Voltar para logon</BackToSignInText>
+        </BackToSignIn>
+      )}
+    </>
+  );
 };
 
 export default SignUp;
